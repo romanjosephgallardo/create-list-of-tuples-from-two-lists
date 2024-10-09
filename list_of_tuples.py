e2740d3a3c4c7aa0d9556abfc50d822dd30be6e3
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-09 09:30:20 +0800

    Added pseudocode.

---
+++ list_of_tuples.py
@@ -0,0 +1,15 @@
+'''
+Using comprehension,create a new list of tuples from two given lists:
+
+### sample calls
+
+list1[1,2,3]
+list2["mark","alice","john]
+
+result: listOfTuple[(1,"mark"),(2,"alice"),(3,"john")] 
+'''
+
+# pseudocode
+# make a list of tuples from two lists
+# make a list of tuples from two lists
+# print the list of tuples
