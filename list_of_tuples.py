7698c9e31b3c69410d7d05d8d3642ac1bb52ff20
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-09 17:01:06 +0800

    Refactor list_of_tuples.py to create a list of tuples from two lists

--- list_of_tuples.py
+++ list_of_tuples.py
@@ -16,7 +16,7 @@ list1 = [1, 2, 3]
 list2 = ["mark", "alice", "john"]
 
 # make a list of tuples from two lists
-list_of_tuples = [f"{list1[element]}, {list2[element]}" for element in range(len(list1))]
+list_of_tuples = [f"({list1[element]}, {list2[element]})" for element in range(len(list1))]
 
 # print the list of tuples
 print(list_of_tuples)
