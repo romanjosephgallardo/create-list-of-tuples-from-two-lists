784dbf43f1c0926b2078f68b3538c6652effcb10
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-09 17:28:22 +0800

    Transform list_of_tuples.py into OOP.

--- list_of_tuples.py
+++ list_of_tuples.py
@@ -10,12 +10,24 @@ result: listOfTuple[(1,"mark"),(2,"alice"),(3,"john")]
 '''
 
 # pseudocode
-# make two lists
+
+# Define a class that takes two lists as input
+class ListOfTuples:
+    # Define a method that takes two lists as input
+    def __init__(self, list_1, list_2):
+        self.list_1 = list_1
+        self.list_2 = list_2
+    
+    # Define a method that creates a list of tuples from two lists
+    def create_list_of_tuples(self):
+        list_of_tuples = [(self.list_1[element], self.list_2[element]) for element in range(len(self.list_1))]
+        # Print the list of tuples
+        print("listOfTuple{}".format(list_of_tuples))
+
+# Make two lists
 list1 = [1, 2, 3]
 list2 = ["mark", "alice", "john"]
 
-# make a list of tuples from two lists
-list_of_tuples = [(list1[element], list2[element]) for element in range(len(list1))]
-
-# print the list of tuples
-print("listOfTuple{}".format(list_of_tuples))
+# Instantiate the class and call the method
+tuple1 = ListOfTuples(list1, list2)
+tuple1.create_list_of_tuples()
