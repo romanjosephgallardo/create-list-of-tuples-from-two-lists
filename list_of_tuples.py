82f083148c1af5d0b5169f2a3c11869947db9de4
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-09 17:29:37 +0800

    Removed docstrings and other comments.

--- list_of_tuples.py
+++ list_of_tuples.py
@@ -1,15 +1,4 @@
-'''
-Using comprehension,create a new list of tuples from two given lists:
-
-### sample calls
-
-list1[1,2,3]
-list2["mark","alice","john]
-
-result: listOfTuple[(1,"mark"),(2,"alice"),(3,"john")] 
-'''
-
-# pseudocode
+# Create a list of tuples from two lists
 
 # Define a class that takes two lists as input
 class ListOfTuples:
