72ef7d260cdd76cde85f2e8506d8f245712af75a
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-09 16:25:35 +0800

    First run of the code.

--- list_of_tuples.py
+++ list_of_tuples.py
@@ -11,5 +11,12 @@ result: listOfTuple[(1,"mark"),(2,"alice"),(3,"john")]
 
 # pseudocode
 # make a list of tuples from two lists
+
+list1 = [1, 2, 3]
+list2 = ["mark", "alice", "john"]
+
 # make a list of tuples from two lists
+list_of_tuples = [f"{list1[element]}, {list2[element]}" for element in range(len(list1))]
+
 # print the list of tuples
+print(list_of_tuples)
